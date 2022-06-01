3c3b08cf7c0b59465c04d45f8c78e19533ddad09
Kristinna97 <kristina.petrova13@gmail.com>
2022-06-01 16:52:22 +0100

    add questions

--- src/questions.js
+++ src/questions.js
@@ -1,6 +1,105 @@
-const questions = [
+
+const astronomy = [
+    {
+    questionText: 'Which is the second planet from the sun?',
+    answerOptions: [
+        { answerText: 'Earth', isCorrect: false },
+        { answerText: 'Jupiter', isCorrect: false },
+        { answerText: 'Venus', isCorrect: true },
+        { answerText: 'Neptune', isCorrect: false },
+    ],
+},
+    {
+    questionText: 'Phobos and Deimos are moons of which planet in our solar system?',
+    answerOptions: [
+        { answerText: 'Mars', isCorrect: true },
+        { answerText: 'Saturn', isCorrect: false },
+        { answerText: 'Venus', isCorrect: false },
+        { answerText: 'Earth', isCorrect: false },
+    ],
+},
+    {
+    questionText: 
+    'What is the name of the galaxy that contains the Earth?',
+    answerOptions: [
+        { answerText: 'Needle Galaxy', isCorrect: false },
+        { answerText: 'Fireworks Galaxy', isCorrect: false },
+        { answerText: 'Milky Way', isCorrect: true },
+        { answerText: 'Medusa Merger', isCorrect: false },
+    ],
+},
+    {
+    questionText: 'The North Star is also known by what other name?',
+    answerOptions: [
+        { answerText: 'Polaris', isCorrect: true },
+        { answerText: 'Sirius', isCorrect: false },
+        { answerText: 'Alpha', isCorrect: false },
+        { answerText: 'Arcturus', isCorrect: false },
+    ],
+},
+    {
+    questionText: 'How may moons does the planet Mars have?',
+    answerOptions: [
+        { answerText: '0', isCorrect: false },
+        { answerText: '1', isCorrect: false },
+        { answerText: '6', isCorrect: false },
+        { answerText: '2', isCorrect: true },
+    ],
+},
+    {
+    questionText: 
+    'Which planet in our solar system has a year which lasts around 84 Earth years?',
+    answerOptions: [
+        { answerText: 'Mars', isCorrect: false },
+        { answerText: 'Pluto', isCorrect: false },
+        { answerText: 'Uranus', isCorrect: true },
+        { answerText: 'Jupiter', isCorrect: false },
+    ],
+},
+    {
+    questionText: 
+    'Selenology is the scientific study of which celestial body?',
+    answerOptions: [
+        { answerText: 'Commets', isCorrect: false },
+        { answerText: 'Moon', isCorrect: true },
+        { answerText: 'Meteors', isCorrect: false },
+        { answerText: 'Sun', isCorrect: false },
+    ],
+}, {
+    questionText: 
+    'How many planets are there in our solar system?',
+    answerOptions: [
+        { answerText: '4', isCorrect: false },
+        { answerText: '6', isCorrect: false },
+        { answerText: '8', isCorrect: true },
+        { answerText: '12', isCorrect: false },
+    ],
+},
+    {
+    questionText: 
+    'Olympus Mons is the highest mountain on which planet in our solar system?',
+    answerOptions: [
+        { answerText: 'Mars', isCorrect: true },
+        { answerText: 'Neptune', isCorrect: false },
+        { answerText: 'Jupiter', isCorrect: false },
+        { answerText: 'Saturn', isCorrect: false },
+    ],
+},   {
+    questionText: 
+    `
+    Which is the fastest rotating planet in our solar system?`,
+    answerOptions: [
+        { answerText: 'Earth', isCorrect: false },
+        { answerText: 'Jupiter', isCorrect: true },
+        { answerText: 'Mars', isCorrect: false },
+        { answerText: 'Neptune', isCorrect: false },
+    ],
+}
+]
+const geography = [
     {
         questionText: 'What is the capital of England?',
+        img:'http://wheelchairtraveling.com/wp-content/gallery/london-attractions/small_london_eye.jpg',
         answerOptions: [
             { answerText: 'New York', isCorrect: false },
             { answerText: 'London', isCorrect: true },
@@ -8,18 +107,9 @@ const questions = [
             { answerText: 'Dublin', isCorrect: false },
         ],
     },
-    
-    {
-        questionText: 'The iPhone was created by which company?',
-        answerOptions: [
-            { answerText: 'Apple', isCorrect: true },
-            { answerText: 'Intel', isCorrect: false },
-            { answerText: 'Amazon', isCorrect: false },
-            { answerText: 'Microsoft', isCorrect: false },
-        ],
-    },
     {
         questionText: 'Which country has the longest coastline?',
+        img:'https://thumbs.dreamstime.com/b/east-end-cayman-islands-aerial-drone-image-island-grand-clear-blue-green-tropical-waters-caribbean-sea-179298373.jpg',
         answerOptions: [
             { answerText: 'Canada', isCorrect: true },
             { answerText: 'Russia', isCorrect: false },
@@ -29,6 +119,7 @@ const questions = [
     },
     {
         questionText: ' Rabat is the capital of which African country?',
+        img:'https://thumbs.dreamstime.com/b/morocco-picturesque-small-village-desert-region-marrakesh-51116395.jpg',
         answerOptions: [
             { answerText: 'Alegria', isCorrect: false },
             { answerText: 'Egypt', isCorrect: false },
@@ -36,7 +127,194 @@ const questions = [
             { answerText: 'Nigeria', isCorrect: false },
         ],
     },
+    {
+        questionText: ' A sabra is a native of which country?',
+        img:'https://www.abercrombiekent.com/-/media/ak/journeys/base-images/search-images/lsgj/middle-east-israel-crossroads-history-western-wall-temple-mount-sr-2020.jpg?sc=0.7&hash=5944344DFE0CF7AA09CE85E3AD36C61D',
+        answerOptions: [
+            { answerText: 'Israel', isCorrect: true },
+            { answerText: 'Palestine', isCorrect: false },
+            { answerText: 'Uruguay', isCorrect: false },
+            { answerText: 'Latvia', isCorrect: false },
+        ],
+    },
+    {
+        questionText: ' Which, by area, is the largest country in South America?',
+        img:'https://media.istockphoto.com/photos/rio-de-janeiro-aerial-picture-id534215078?k=20&m=534215078&s=612x612&w=0&h=m_f-fdLvL5w4IWPMBckpOD8TQVweNnVXkhlbHTIwYjo=',
+        answerOptions: [
+            { answerText: 'Venezuela', isCorrect: false },
+            { answerText: 'Chile', isCorrect: false },
+            { answerText: 'Argentina', isCorrect: false },
+            { answerText: 'Brazil', isCorrect: true },
+        ],
+    },
+    {
+        questionText: 'At 2,277 feet, Mount Botrange is the highest point of which European country?',
+        img:'https://lets-travel-more.com/wp-content/uploads/2018/08/Pearl-of-Ardennes-1.jpg',
+        answerOptions: [
+            { answerText: 'Germany', isCorrect: false },
+            { answerText: 'Norway', isCorrect: false },
+            { answerText: 'Belgium', isCorrect: true },
+            { answerText: 'Ukraine', isCorrect: false },
+        ],
+    },
+    {
+        questionText: ' Which river flows through Glasgow?',
+        img:'https://cdn.getyourguide.com/img/location/5cab26735785f.jpeg/68.jpg',
+        answerOptions: [
+            { answerText: 'Rhine', isCorrect: false },
+            { answerText: 'Congo River', isCorrect: false },
+            { answerText: 'Danube', isCorrect: false },
+            { answerText: 'Clyde', isCorrect: true },
+        ],
+    },
+    {
+        questionText: ' Which sea in Northern Europe is bounded by several countries including Sweden, Finland, Poland and Germany?',
+        img:'https://thumbs.dreamstime.com/b/small-freshwater-incupe-river-flows-baltic-sea-near-small-freshwater-incupe-river-flows-baltic-sea-near-227147280.jpg',
+        answerOptions: [
+            { answerText: 'Adriatic Sea', isCorrect: false },
+            { answerText: 'The Baltic Sea', isCorrect: true },
+            { answerText: 'Celtic Sea', isCorrect: false },
+            { answerText: 'Molucca Sea', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'Which city is also known as ‘The Eternal City’?',
+        img:'https://ik.imagekit.io/tvlk/xpe-asset/AyJ40ZAo1DOyPyKLZ9c3RGQHTP2oT4ZXW+QmPVVkFQiXFSv42UaHGzSmaSzQ8DO5QIbWPZuF+VkYVRk6gh-Vg4ECbfuQRQ4pHjWJ5Rmbtkk=/4953456576880/Small-Group-Ancient-Rome-and-Colosseum-Underground-Tour-b48aa8e6-374b-4b07-8249-5301a82e09e8.jpeg?_src=imagekit&tr=c-at_max,h-512,q-60,w-720',
+        answerOptions: [
+            { answerText: 'Amsterdam', isCorrect: false },
+            { answerText: 'Paris', isCorrect: false },
+            { answerText: 'Rome', isCorrect: true },
+            { answerText: 'Sofia', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'What is the official currency of India?',
+        img:'https://maps.lib.utexas.edu/maps/cia16/india_sm_2016.gif',
+        answerOptions: [
+            { answerText: 'Dolar', isCorrect: false },
+            { answerText: 'Indian rupee', isCorrect: true },
+            { answerText: 'Renminbi', isCorrect: false },
+            { answerText: 'Euro', isCorrect: false },
+        ],
+    },
    
 ];
 
-module.exports = {questions}
+const history = [
+    {
+        questionText: 'How many years did the 100 years war last?',
+        img:'https://www.ducksters.com/history/middle_ages/hundred_years_war_agincourt.jpg',
+        answerOptions: [
+            { answerText: '97', isCorrect: false },
+            { answerText: '116', isCorrect: true },
+            { answerText: '110', isCorrect: false },
+            { answerText: '100', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'In which year was John F. Kennedy assassinated?',
+        img:'https://upload.wikimedia.org/wikipedia/commons/5/5e/John_F._Kennedy%2C_White_House_photo_portrait%2C_looking_up.jpg',
+        answerOptions: [
+            { answerText: '1971', isCorrect: false },
+            { answerText: '1962', isCorrect: false },
+            { answerText: '1970', isCorrect: false },
+            { answerText: '1963', isCorrect: true },
+        ],
+    },
+    {
+        questionText: 'How many days in a week were there in ancient Roman times?',
+        img:'https://i.guim.co.uk/img/media/b1b5b198c604262b73fa68c36fc59c4b77f78228/0_122_3072_1844/master/3072.jpg?width=465&quality=45&auto=format&fit=max&dpr=2&s=61c33bf9068faace2b16c4370bccde96',
+        answerOptions: [
+            { answerText: '9', isCorrect: false },
+            { answerText: '6', isCorrect: false },
+            { answerText: '8', isCorrect: true },
+            { answerText: '7', isCorrect: false },
+        ],
+    },
+    {
+        questionText: ' How many days was William Harrison President of the United States?',
+        img:'https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/Rembrandt_Peale_-_William_Henry_Harrison_-_Google_Art_Project.jpg/170px-Rembrandt_Peale_-_William_Henry_Harrison_-_Google_Art_Project.jpg',
+        answerOptions: [
+            { answerText: '28', isCorrect: false },
+            { answerText: '32', isCorrect: true },
+            { answerText: '30', isCorrect: false },
+            { answerText: '41', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'When did the first Space Shuttle go into space?',
+        img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3V6K-cBVSDhqyvpJKWQjS4fpFnHsag5HKFc9j-PmzQv_nH-FU36d8fSgzpKNyhimT8gM&usqp=CAU',
+        answerOptions: [
+            { answerText: '1980', isCorrect: false },
+            { answerText: '1990', isCorrect: false },
+            { answerText: '1981', isCorrect: true },
+            { answerText: '1995', isCorrect: false },
+        ],
+    }
+]
+const biology = [
+    {
+        questionText: 'Which of the following words refers to a baby shark?',
+        img:'https://www.news-medical.net/image.axd?picture=2021%2F11%2Fshutterstock_166336202.jpg',
+        answerOptions: [
+            { answerText: ' A sharkling', isCorrect: false },
+            { answerText: 'A pup', isCorrect: true },
+            { answerText: ' A fry', isCorrect: false },
+            { answerText: 'A cub', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'Which is the most common element that accounts for more than 60 per cent of the human body?',
+        img:'http://cen.acs.org/content/dam/cen/91/25/09125-books-periodictable1.jpg',
+        answerOptions: [
+            { answerText: 'Carbon', isCorrect: false },
+            { answerText: ' Hydrogen', isCorrect: false },
+            { answerText: 'Oxygen', isCorrect: true },
+            { answerText: 'Nitrogen', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'Which of the following terms refers to a DNA change that leads to a new characteristic?',
+        img:'https://www.syfy.com/sites/syfy/files/styles/blog_post_featured_media_mobile/public/2022/01/gettyimages-1339204507.jpg?h=b6717701',
+        answerOptions: [
+            { answerText: ' Defection', isCorrect: false },
+            { answerText: 'Revolution', isCorrect: false },
+            { answerText: 'Destruction', isCorrect: false },
+            { answerText: 'Mutation', isCorrect: true },
+        ],
+    },
+    {
+        questionText: 'Photosynthesis mostly occurs in which part of plants?',
+        img:'https://www.thompson-morgan.com/static-images/tandm/static-articles/plants-for-shade/20211210_tm_collection_of_hostas.jpg',
+        answerOptions: [
+            { answerText: 'Leaves', isCorrect: true },
+            { answerText: 'Root', isCorrect: false },
+            { answerText: 'Bark', isCorrect: false },
+            { answerText: 'Branch', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'Mycology is the branch of biology that studies what?',
+        img:'https://cdn.britannica.com/21/153021-138-480FDB6F/role-fungi-decomposition-forest-litter.jpg?w=800&h=450&c=crop',
+        answerOptions: [
+            { answerText: 'Animals', isCorrect: false },
+            { answerText: 'Viruses', isCorrect: false },
+            { answerText: 'Fungi', isCorrect: true },
+            { answerText: 'Cells', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'Which of the following big cats is the fastest animal on land?',
+        img:'https://c.ndtvimg.com/2020-01/nfb3k5nc_african-cheetah-unsplash_625x300_28_January_20.jpg',
+        answerOptions: [
+            { answerText: 'Puma', isCorrect: false },
+            { answerText: 'Leopard', isCorrect: false },
+            { answerText: 'Cheetah', isCorrect: true },
+            { answerText: 'Lynx', isCorrect: false },
+        ],
+    }
+]
+
+const questions = []
+
+module.exports = { questions, astronomy ,geography, history, biology }
